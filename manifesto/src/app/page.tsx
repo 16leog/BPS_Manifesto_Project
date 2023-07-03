d79c7cd74194fca92dfce686aae20273cdbd02de
Leonel Gonzalez <16leog@gmail.com>
2023-07-03 16:55:54 -0500

    imported font for navbar logo

---
+++ manifesto/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import { Old_Standard_TT, Open_Sans } from "next/font/google";
+
+export default function Home() {
+  return (
+    <html lang="en" className="sm:scroll-smooth">
+      <body></body>
+    </html>
+  );
+}
---
+++ manifesto/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        NAVBAR_COLOR: "#1f1f39",
+        HOMEPAGE_BGCOLOR: "#19192e",
+      },
+      fontFamily: {
+        oldStandard: "--font-old-standard-tt",
+      },
+    },
+  },
+  plugins: [],
+};
