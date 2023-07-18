8f1b38659533a34be322fb0a7369c4210cd76bc1
Leonel Gonzalez <16leog@gmail.com>
2023-07-18 16:56:27 -0500

    feat: as close as i can get before just rendering navbar in each page to get around bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client';
-import { Inter, Montserrat } from 'next/font/google';
+import {
+  Inter,
+  Montserrat,
+  Old_Standard_TT,
+  Open_Sans,
+} from 'next/font/google';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import BackButton from '../../public/back-button.svg';
 import deleteButton from '../../public/delete-button.svg';
 import editButton from '../../public/edit-button.svg';
 import ManifestoBanner from '../../public/manifesto-banner.svg';
+import manifestoWebSkyline from '../../public/manifestoWebSkyline.svg';
 import FormData from '../../types';
 import NoOne from './components/Homescreen/NoOne';
 
@@ -15,10 +22,22 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
+const oldStandard = Old_Standard_TT({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-old-standard-tt',
+});
+
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: '500',
   variable: '--font-montserrat',
+});
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--font-open-sans',
+  weight: '600',
 });
 
 // signinList;
@@ -65,69 +84,104 @@ export default function Home() {
 
   return (
     <html lang="en" className="sm:scroll-smooth">
-      <body className="min-h-screen bg-HOMEPAGE_BGCOLOR">
-        <div className="">
+      <body className="min-h-screen bg-HOMEPAGE_BGCOLOR sm:bg-NAVBAR_COLOR">
+        <div className="block sm:hidden">
           <Image
             className="w-full mt-9"
             src={ManifestoBanner}
             alt="ManifestoBanner"
           />
         </div>
-        <div className="w-full flex flex-col  h-72 sm:h-58 ">
-          {formDataList.length === 0 ? (
-            <NoOne />
-          ) : (
-            <>
-              <div className="flex items-center justify-around mt-14 mb-8">
-                <h1 className="text-white">
-                  <a className={montserrat.className}>
-                    Sign in at the registry.
-                  </a>
-                </h1>
-              </div>
-              <div className="overflow-y-auto">
-                {formDataList.map((formData, index) => (
-                  <div key={index} className=" mx-1 justify-center">
-                    <div className=" text-white flex flex-row justify-between items-center w-11/12">
-                      <h1 className="ml-8">
-                        <a className={montserrat.className}>
-                          {formData.fullName}
-                        </a>
-                      </h1>
-                      <div className="w-20">
-                        <button
-                          className="pr-4 w-auto h-auto"
-                          onClick={() => handleEdit(formData.id)}
-                        >
-                          <Image src={editButton} alt={'edit button'} />
-                        </button>
-                        <button
-                          className="w-auto h-auto"
-                          onClick={() => handleDelete(index)}
-                        >
-                          <Image src={deleteButton} alt={'delete button'} />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="ml-8 max-md:w-5/6 h-px m-3 bg-gray-400 self-center sm:w-7/12"></div>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </div>
-        <div className="flex justify-center mt-44">
-          <button
-            onClick={() => {
-              localStorage.removeItem('editingFormId');
-              router.push('/signin');
-            }}
-            className="bg-MANIFESTO_COLOR w-44 h-12"
-          >
-            <h1 className="text-NAVBAR_COLOR">
-              <a className={inter.className}>Sign In</a>
-            </h1>
+        <div className="hidden sm:block mb-auto">
+          <button onClick={() => router.back()}>
+            <div className="flex items-center ml-4 mt-4">
+              <Image src={BackButton} alt="BackButton" />
+              <span className="ml-2 font-medium text-MANIFESTO_COLOR font">
+                <a className={montserrat.className}>Back</a>
+              </span>
+            </div>
           </button>
+        </div>
+        <div className="relative hidden sm:block max-w-[1350px] mx-auto mt-auto  items-center">
+          <div className="absolute left-0 right-0 top-0 bottom-8 z-10 flex flex-col items-center justify-center ">
+            <h1 className="text-MANIFESTO_COLOR text-75px  sm:relative max-sm:text-center ">
+              <a className={oldStandard.className}>M</a>
+            </h1>
+            <p className={openSans.className}>
+              <a className="text-white text-30px">Manifesto</a>
+            </p>
+          </div>
+          <Image
+            className="object-center"
+            src={manifestoWebSkyline}
+            alt="manifestoWebSkyline"
+          />
+        </div>
+
+        <div className="bg-HOMEPAGE_BGCOLOR max-w-[1350px] mx-auto">
+          <div className=" max-w-[540px] h-[616px] mx-auto mb-28">
+            <div className="flex flex-col justify-center h-[350px] ">
+              {formDataList.length === 0 ? (
+                <NoOne />
+              ) : (
+                <>
+                  <div className="flex items-center justify-around mt-12 mb-8">
+                    <h1 className="text-white text-xl">
+                      <a className={montserrat.className}>
+                        Sign in at the registry.
+                      </a>
+                    </h1>
+                  </div>
+                  <div className="justify-center overflow-y-auto">
+                    <div className="overflow-y-auto ">
+                      {formDataList.map((formData, index) => (
+                        <div key={index} className="justify-center mx-8">
+                          <div className=" text-white flex flex-row justify-between items-center w-11/12">
+                            <h1 className="ml-8">
+                              <a className={montserrat.className}>
+                                {formData.fullName}
+                              </a>
+                            </h1>
+                            <div className="w-20">
+                              <button
+                                className="pr-4 w-auto h-auto"
+                                onClick={() => handleEdit(formData.id)}
+                              >
+                                <Image src={editButton} alt={'edit button'} />
+                              </button>
+                              <button
+                                className="w-auto h-auto"
+                                onClick={() => handleDelete(index)}
+                              >
+                                <Image
+                                  src={deleteButton}
+                                  alt={'delete button'}
+                                />
+                              </button>
+                            </div>
+                          </div>
+                          <div className="mx-8 max-md:w-5/6 h-px m-3 bg-gray-400 self-center sm:w-11/12"></div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="flex justify-center max-sm:mt-44 md:mt-20">
+              <button
+                onClick={() => {
+                  localStorage.removeItem('editingFormId');
+                  router.push('/signin');
+                }}
+                className="bg-MANIFESTO_COLOR w-44 h-12"
+              >
+                <h1 className="text-NAVBAR_COLOR">
+                  <a className={inter.className}>Sign In</a>
+                </h1>
+              </button>
+            </div>
+          </div>
         </div>
       </body>
     </html>
